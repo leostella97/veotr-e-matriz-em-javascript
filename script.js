e3f813599b752068d4770d51d39527861ea3f39e
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2024-09-22 14:08:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//também chamado de array
+
+//primeiro, para criar um vetor, cria uma variável e depois, atribuia valores ao vetor
+vetor = ["um", "dois", "três"]
+
+//para criar uma matriz, você cria como se fosse uma "tabela" tendo as linhas e colunas, x e y, linha = x / coluna = y
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+//se eu quiser o 1 que é a primeira linha e a primeira oluna, eu exibo o 0 e 0, porque array sempre cmeça do 0 e, claro, indicando a array e os locais
+console.log (matriz[0][0])
+
+//se eu quiser o 9, que é a terceira linha e a terceira coluna, exibo o [2][2]
+console.log (matriz[2][2])
